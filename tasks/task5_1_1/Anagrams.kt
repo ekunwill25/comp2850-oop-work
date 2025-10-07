6f8acbb99d43186f2a198fbfbf5469de9df71f0d
e. <150349678+ekunwill25@users.noreply.github.com>
2025-10-07 10:56:15 +0000

    edit10

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,23 @@
-// Task 5.1.1: anagram checking using a function
+fun anagrams(name1: String, name2: String): Boolean {
+    if (name1.length != name2.length) {
+        return false
+    }
+    val sortedname1 = name1.lowercase().toList().sorted()
+    val sortedname2 = name2.lowercase().toList().sorted()
+    return sortedname1 == sortedname2
+
+}
+
+fun main() {
+    println("Enter the first word:")
+    val word1 = readln()
+    println("Enter the second word:")
+    val word2 = readln()
+    if (anagrams(word1, word2)) {
+        println("These words are anagrams!")
+    }
+    else {
+        println("Oh HN man, these are not anagrams")
+
+    }
+}
