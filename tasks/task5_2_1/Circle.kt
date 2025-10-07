26b7801f843755dabde7ffe0a93c0a76ef534a2b
e. <150349678+ekunwill25@users.noreply.github.com>
2025-10-07 11:50:59 +0000

    edit11

--- tasks/task5_2_1/Circle.kt
+++ tasks/task5_2_1/Circle.kt
@@ -1,1 +1,21 @@
-// Task 5.2.1: geometric properties of circles
+import kotlin.math.PI
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun circleArea(radius: Double) = PI * radius.pow(2)
+fun circlePerim(radius: Double) = 2 * PI * radius
+fun readDouble(val1: String): Double{
+    println("Please enter the value:")
+    return readln().toDouble()
+}
+
+fun main() {
+    println("Welcome to the circle calculator!")
+    println("Please type if you are ready to proceed:")
+    val val1 = readln()
+    val radius = readDouble(val1)
+    val area = circleArea(radius)
+    val perim = circlePerim(radius)
+    println("The area of your circle is %.2f".format(area))
+    println("The perimeter of your circle is %.2f".format(perim))
+}
--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,1 +1,9 @@
-// Task 5.2.2: conversion of marks into grades, using a function
+fun grade(mark: Int) = when (mark) {
+    in 0..39   -> "Fail"
+    in 40..69  -> "Pass"
+    in 70..100 -> "Distinction"
+    else       -> "?"
+}
+fun main() {
+    for (i in )
+}
