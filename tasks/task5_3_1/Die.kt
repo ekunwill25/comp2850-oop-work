ec2e3b2d02c3c29573451d41b77ed1ffd42b0ac3
e. <150349678+ekunwill25@users.noreply.github.com>
2025-10-21 12:21:22 +0000

    edit14

---
+++ tasks/task5_3_1/Die.kt
@@ -0,0 +1,17 @@
+import kotlin.random.Random
+fun rollDie(sides: Int = 6) {
+    if (sides in setOf(4,6,8,10,12,20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled a $result")
+    }
+    else {
+        println("Error: A d$sides does not exist.")
+    }
+}
+
+fun main() {
+    println("Please enter the number of sides that your die will have:")
+    val sides = readln().toInt()
+    return rollDie(sides)
+}
