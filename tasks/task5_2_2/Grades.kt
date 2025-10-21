ec28ab0cad69eb9bbddd649d8d8bfce916ec64c1
e. <150349678+ekunwill25@users.noreply.github.com>
2025-10-21 11:22:21 +0000

    edit

--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,9 +1,19 @@
-fun grade(mark: Int) = when (mark) {
-    in 0..39   -> "Fail"
-    in 40..69  -> "Pass"
-    in 70..100 -> "Distinction"
-    else       -> "?"
-}
-fun main() {
-    for (i in )
+fun main(args: Array<String>) {
+    fun grade(mark: Int): String{
+        return when (mark) {
+            in 0..39   -> "Fail"
+            in 40..69  -> "Pass"
+            in 70..100 -> "Distinction"
+            else       -> "?"
+        }
+    }
+
+    for (arg in args) {
+        val score = arg.toIntOrNull()
+        if (score == null) {
+            println("Put a real number")
+        } else {
+            println("$score is a ${grade(score)}")
+        }
+    }
 }
